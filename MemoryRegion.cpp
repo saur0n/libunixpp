eb1d7b2761d441933c38c20c685e724930b34d5c
Sauron <git@saur0n.science>
2025-04-13 22:31:51 +0300

    Added support for protection keys

--- MemoryRegion.cpp
+++ MemoryRegion.cpp
@@ -2,7 +2,7 @@
  *  libunix++: C++ wrapper for Linux system calls
  *  Memory operations
  *  
- *  © 2019—2021, Sauron <libunixpp@saur0n.science>
+ *  © 2019—2025, Sauron <libunixpp@saur0n.science>
  ******************************************************************************/
 
 #include <sys/mman.h>
@@ -10,6 +10,18 @@
 #include "MemoryRegion.hpp"
 
 using namespace upp;
+
+/******************************************************************************/
+
+ProtectionKey::ProtectionKey(unsigned int accessRights) :
+        pkey(pkey_alloc(0, accessRights)) {}
+
+ProtectionKey::ProtectionKey(unsigned int flags, unsigned int accessRights) :
+        pkey(pkey_alloc(flags, accessRights)) {}
+
+ProtectionKey::~ProtectionKey() {
+    pkey_free(pkey);
+}
 
 MemoryRegion::MemoryRegion(void * address, size_t length) :
         address(address), length(length) {}
@@ -24,4 +36,8 @@ void MemoryRegion::incore(unsigned char * result) {
 
 void MemoryRegion::protect(int prot) {
     NORMAL_OP_WRAPPER(mprotect(address, length, prot));
+}
+
+void MemoryRegion::protect(int prot, const ProtectionKey &pkey) {
+    NORMAL_OP_WRAPPER(pkey_mprotect(address, length, prot, pkey.pkey));
 }
--- MemoryRegion.hpp
+++ MemoryRegion.hpp
@@ -2,13 +2,37 @@
  *  libunix++: C++ wrapper for Linux system calls
  *  Memory operations
  *  
- *  © 2019—2021, Sauron <libunixpp@saur0n.science>
+ *  © 2019—2025, Sauron <libunixpp@saur0n.science>
  ******************************************************************************/
 
 #ifndef __UNIXPP_MEMORYREGION_HPP
 #define __UNIXPP_MEMORYREGION_HPP
 
+#include <sys/mman.h>
+
 namespace upp {
+
+/**/
+class ProtectionKey {
+    friend class MemoryRegion;
+public:
+    /**/
+    static const int DISABLE_ACCESS;
+    /**/
+    static const int DISABLE_WRITE;
+    /** Allocate a protection key **/
+    explicit ProtectionKey(unsigned int accessRights);
+    /** Allocate a protection key **/
+    ProtectionKey(unsigned int flags, unsigned int accessRights);
+    /** Free the protection key **/
+    ~ProtectionKey();
+    
+private:
+    ProtectionKey(const ProtectionKey &other)=delete;
+    ProtectionKey &operator =(const ProtectionKey &other)=delete;
+    
+    int pkey;
+};
 
 /** A contiguous region of memory **/
 class MemoryRegion {
@@ -21,12 +45,14 @@ public:
     void incore(unsigned char * result);
     /** Set protection on a region of memory **/
     void protect(int prot);
+    /** Set protection on a region of memory **/
+    void protect(int prot, const ProtectionKey &pkey);
     /** Returns the initial address **/
     void * getAddress() const { return address; }
     /** Returns the length in bytes **/
     size_t getLength() const { return length; }
     
-private:
+protected:
     void * address;
     size_t length;
 };
